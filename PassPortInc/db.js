c13d81279d8f1aa9ee8da19d5704580491547beb
Keila Gonzalez <keilagonzalezlopez15@gmail.com>
2025-11-02 20:34:12 -0300

    Agrega base de datos inicial, usuario

--- PassPortInc/db.js
+++ PassPortInc/db.js
@@ -1,4 +1,4 @@
-const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require("sqlite3").verbose(); // Activa mensajes si ocurre un error o advertencia
 
 const db = new sqlite3.Database("./database/passport.db");
 
@@ -7,5 +7,7 @@ db.inicialize(() => {
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         email TEXT UNIQUE,
         password TEXT,
-        )`)
-})
+        rol TEXT DEFAULT 'usuario'
+        )`);
+});
+module.exports = db;
